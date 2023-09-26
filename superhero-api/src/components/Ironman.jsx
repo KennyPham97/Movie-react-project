932a6be5a82cd1ea00c29bcc10bfa2d2a2e2c7f2
COR Coding <corcoding@Coding-1.attlocal.net>
2023-09-25 23:08:49 -0400

    added additional information to display on poster click, polished up the styling of the site

--- superhero-api/src/components/Ironman.jsx
+++ superhero-api/src/components/Ironman.jsx
@@ -1,95 +1,34 @@
-// import React, { useState, useEffect } from 'react'
-// import './Ironman.css'
-
-// const Ironman = () => {
-//     const [searchQuery, setSearchQuery] = useState("")
-//     const [movies, setMovies] = useState([])
-//     const [selectedMovie, setSelectedMovie] = useState(null)
-    
-//     useEffect(() => {
-//       fetchMovieData('Iron-Man')
-//     }, [])
-    
-//     const fetchMovieData = async (query) => {
-//     let response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=f9befeac`)
-//     let data = await response.json()
-//     console.log(data)
-//     setMovies(data.Search || [])
-//   };
-
-
-  
-//     const handleInputChange = (e) => {
-//       setSearchQuery(e.target.value)
-//     };
-    
-//     const handleSearch = () => {
-//         fetchMovieData(searchQuery)
-//     };
-//     const handleMovieClick = (movie) => {
-//       setSelectedMovie(movie)
-//     };
-    
-//   return (
-//     <div>
-//       <div>
-//         <h1 className='header'>MOVIES.KP</h1>
-//         <div className='input'>
-//         <input type="text"value={searchQuery}onChange={handleInputChange}/>
-//         <button onClick={handleSearch}>Search</button>
-//         </div>
-//       </div>
-      
-//       <ul className="movie-list">
-//         {movies.map((movie, index) => {
-//           return (
-//             <li key={index} className="movie-item">
-//               <h2>{movie.Title}</h2>
-
-//               <img src={movie.Poster} alt={movie.Title} onClick={() => handleMovieClick(movie)}/>
-//               {selectedMovie && selectedMovie.imdbID === movie.imdbID && (
-//                 <div>
-//                   <p>Year: {selectedMovie.Year}</p>
-//                   <p>Type: {selectedMovie.Type}</p>
-//                 </div>
-//               )}
-//             </li>
-          
-//           )
-//         })}
-//       </ul>
-//     </div>
-//   )
-// }
-// export default Ironman;
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react'
 import './Ironman.css'
 
 const Ironman = () => {
   const [searchQuery, setSearchQuery] = useState('')
   const [movies, setMovies] = useState([])
-  const [selectedMovie, setSelectedMovie] = useState(null)
+  const [selectedMovies, setSelectedMovies] = useState({})
+  
 
   useEffect(() => {
-    fetchMovieData('Iron-Man')
+    fetchMovieData('Star-Wars')
   }, []);
 
   const fetchMovieData = async (query) => {
-    let response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=f9befeac`);
-    let data = await response.json();
-    console.log(data);
-    setMovies(data.Search || []);
+    let response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=f9befeac`)
+    let data = await response.json()
+    console.log(data)
+    setMovies(data.Search || [])
+
+    if (!data.Search || data.Search.length === 0) {
+      window.location.reload()
+    }
+  };
+
+  const fetchSelectedMovieData = async (imdbID) => {
+    let response = await fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=f9befeac`)
+    let data = await response.json()
 
     
-    if (!data.Search || data.Search.length === 0) {
-      window.location.reload();
-    }
+    setSelectedMovies({ ...selectedMovies, [imdbID]: data })
+    console.log(data);
   };
 
   const handleInputChange = (e) => {
@@ -100,38 +39,47 @@ const Ironman = () => {
     fetchMovieData(searchQuery)
   };
 
-  const handleMovieClick = (movie) => {
-    setSelectedMovie(movie);
-  };
-
   return (
     <div>
       <div>
-        <h1 className="header">MOVIES.KP</h1>
+        <h1 className="header">MOVIES.KP <img className='logo' src = "https://www.seekpng.com/png/full/760-7601854_popcornanimater-popcorn-gif-sticker.png"/></h1>
+
         <div className="input">
-          <input type="text" value={searchQuery} onChange={handleInputChange} />
-          <button onClick={handleSearch}>Search</button>
+          <input className='input-field' type="text" value={searchQuery} onChange={handleInputChange} />
+          <button className= 'search-button' onClick={handleSearch}>Search</button>
         </div>
       </div>
 
       <ul className="movie-list">
-        {movies.length === 0 ? (<p>No Movies Found</p>) : (movies.map((movie, index) => {
-            return (
-              <li key={index} className="movie-item">
-                <h2>{movie.Title}</h2>
+        {movies.map((movie, index) => {
+          return (
+            <li key={index} className="movie-item">
+              <h2>{movie.Title}</h2>
 
-                <img src={movie.Poster} alt={movie.Title} onClick={() => handleMovieClick(movie)} />
-                {selectedMovie && selectedMovie.imdbID === movie.imdbID && (
+                <img src={movie.Poster} alt={movie.Title} onClick={() => fetchSelectedMovieData(movie.imdbID)} />
+
+                
+                {selectedMovies[movie.imdbID] && (
                   <div>
-                    <p>Year: {selectedMovie.Year}</p>
-                    <p>Type: {selectedMovie.Type}</p>
+                    <p>Year: {selectedMovies[movie.imdbID].Year}</p>
+                    <p>Genre: {selectedMovies[movie.imdbID].Genre}</p>
+                    <p>Runtime: {selectedMovies[movie.imdbID].Runtime}</p>
+                    <p>Rated: {selectedMovies[movie.imdbID].Rated}</p>
+                    <p>Actors: {selectedMovies[movie.imdbID].Actors}</p>
+                    <p>Director: {selectedMovies[movie.imdbID].Director}</p>
+                    <p>Plot: {selectedMovies[movie.imdbID].Plot}</p>
+                    <p>imdbRating: {selectedMovies[movie.imdbID].imdbRating}</p>
+                    
                   </div>
                 )}
               </li>
             )
           })
-        )}
+        }
       </ul>
+      <footer>
+        <p className='footer'>2023 Movies.KP. All Rights Reserved to Kenny Pham.</p>
+      </footer>
     </div>
   )
 }
